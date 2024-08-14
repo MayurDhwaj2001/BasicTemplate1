c395142cbddf396fcc2ae2ec5371708f1c66fd49
Mayur Dhwaj <mayurdhwaj1315@gmail.com>
2024-08-14 23:50:35 +0530

    up button added

--- script.js
+++ script.js
@@ -25,3 +25,32 @@ window.addEventListener("scroll", () => {
 });
 
 // Navbar Script End
+
+// Go to Top button
+
+// Get the button Begin
+var upButton = document.getElementById("upButton");
+
+// When the user scrolls down 100px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    upButton.style.display = "block";
+  } else {
+    upButton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+upButton.onclick = function () {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+};
+
+// Go To Top Button End
